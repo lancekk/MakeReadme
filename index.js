c88661f46567713218159d3038125d2fd008ac77
Lance Kelley <lancekkcodes@gmail.com>
2021-10-10 20:13:01 -0500

    initial questions

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
 // TODO: Include packages needed for this application
+let gm = require('./utils/generateMarkdown');
+let inq = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    name: "Title",
+    type: "input",
+  },
+  {
+    name: "Description",
+    type: "input",
+  },
+  {
+    name: "Installation",
+    type: "input",
+  },
+  {
+    name: "Usage",
+    type: "input",
+  },
+  {
+    name: "Contributing",
+    type: "input",
+  },
+  {
+    name: "Testing",
+    type: "input",
+  },
+  {
+    name: "License",
+    type: "list",
+    choices: [
+    ],
+  },
+  {
+    name: "Github",
+    type: "input",
+  },
+  {
+    name: "Email",
+    type: "input",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
